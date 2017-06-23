404eea8b9d56482e9e5b8eab8ac265e7ad464d64
Jeff Registre <jeffreyregist365@gmail.com>
2017-06-23 11:40:33 -0400

    Added news method to misc db service. Converted news component to using the service. Now all done converting components from using faker to using service for their data.

--- app-front/src/app/components/main/aside/news/news.component.ts
+++ app-front/src/app/components/main/aside/news/news.component.ts
@@ -2,6 +2,10 @@ import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {NewsItem} from '../../../../data/side.interfaces';
 import * as faker from 'faker';
 import 'jquery';
+import {MiscDatabaseService} from '../../../../services/misc-database.service';
+
+const REFRESH_TIME = 10000;
+const CHECK_DELAY = 1000;
 
 @Component({
   selector: 'app-news',
@@ -14,18 +18,26 @@ export class NewsComponent implements AfterViewInit {
   newsItems: NewsItem[] = [];
   showScrollIndicator = true;
 
-  constructor() {
-    for (let i = 0; i < 5; i++) {
-      this.newsItems.push({
-        title: faker.lorem.words(6),
-        shortDescription: faker.lorem.sentences(2),
-        url: faker.internet.url()
-      });
-    }
+  private db: MiscDatabaseService;
+  private timer = REFRESH_TIME;
+
+  constructor(db: MiscDatabaseService) {
+    this.db = db;
+    this.onRefreshNews();
+    console.log(REFRESH_TIME);
+    console.log(typeof REFRESH_TIME);
+    setInterval(this.onAutoRefresh.bind(this), CHECK_DELAY);
   }
 
-  onRefreshNews(event): void {
-    event.preventDefault();
+  onRefreshNews(event?: any): void {
+    if (event) {
+      event.preventDefault();
+      this.timer = REFRESH_TIME;
+    }
+
+    this.db.news.then(value => {
+      this.newsItems = value;
+    });
   }
 
   ngAfterViewInit(): void {
@@ -33,6 +45,14 @@ export class NewsComponent implements AfterViewInit {
     content.scroll(() => {
       this.showScrollIndicator = content[0].scrollHeight - content.scrollTop() !== content.height();
     });
+  }
+
+  private onAutoRefresh(): void {
+    this.timer -= CHECK_DELAY;
+    if (this.timer <= 0) {
+      this.onRefreshNews();
+      this.timer = REFRESH_TIME;
+    }
   }
 
 }
--- app-front/src/app/services/misc-database.service.ts
+++ app-front/src/app/services/misc-database.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {BaseDatabaseService} from './BaseDatabaseService';
-import {Credit, CreditSummary} from '../data/side.interfaces';
+import {Credit, CreditSummary, NewsItem} from '../data/side.interfaces';
 import * as faker from 'faker';
 
 @Injectable()
@@ -18,11 +18,24 @@ export class MiscDatabaseService extends BaseDatabaseService {
       message: faker.lorem.paragraph(2),
       credits
     };
-
     resolve({
       credits: creditSummary
     });
   });
+
+  public get news(): Promise<NewsItem[]> {
+    return new Promise((resolve, reject) => {
+      const newsItems: NewsItem[] = [];
+      for (let i = 0; i < 5; i++) {
+        newsItems.push({
+          title: faker.lorem.words(6),
+          shortDescription: faker.lorem.sentences(2),
+          url: faker.internet.url()
+        });
+      }
+      resolve(newsItems);
+    });
+  }
 
   public credits: Promise<CreditSummary> = this.makeFromSource(this.source, value => {
     return value.credits;
