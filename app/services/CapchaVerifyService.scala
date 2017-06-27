40c36f049276ab300210a408fcc39726eca6ee66
Jeff Registre <jeffreyregist365@gmail.com>
2017-06-27 11:59:33 -0400

    Moved the secret loading to a lazy val in the class so it doesn't get loaded every time.

--- app/services/CapchaVerifyService.scala
+++ app/services/CapchaVerifyService.scala
@@ -2,8 +2,9 @@ package services
 
 import javax.inject.Inject
 
+import play.api.Logger
 import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.json.{JsObject, JsString, Json}
+import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import services.Constants.ServerConfig
 
@@ -24,50 +25,58 @@ trait CapchaVerifyService {
   def verify(capChaToken: String): Future[Try[Boolean]]
 }
 
+/**
+  * An  implementation of the capcha verify service.
+  *
+  * @param configLoader - Has a dependency on the config load to the secret.
+  * @param ws           - Has a dependency on [[WSClient]] to make the verification request.
+  */
 class QueryingCapchaService @Inject()(configLoader: ConfigLoader, ws: WSClient) extends CapchaVerifyService {
+
+  private val logger = Logger(getClass)
+
+  private lazy val loadSecret: Future[Try[String]] = configLoader.loadObject(ServerConfig._1, Some(ServerConfig._2)).map {
+    case Success(config) =>
+      Try({
+        (config \ "capchaSecret").as[String]
+      })
+    case Failure(t) =>
+      Failure(t)
+  }
+
   /**
     * Verifies a recapcha token.
     *
     * @param capChaToken - The capcha token to verify.
     * @return - An attempt at verifying a token.
     */
-  override def verify(capChaToken: String): Future[Try[Boolean]] = configLoader.loadObject(ServerConfig._1, Some(ServerConfig._2)).map {
-    case Success(config) =>
-      val secretTry: Try[String] = Try({
-        (config \ "capchaSecret").as[String]
-      })
+  override def verify(capChaToken: String): Future[Try[Boolean]] = loadSecret.map {
+    case Success(secret) =>
+      val request = ws
+        .url("https://www.google.com/recaptcha/api/siteverify")
+        .withHeaders(
+          "Accept" -> "application/json",
+          "Content-type" -> "application/x-www-form-urlencoded"
+        )
 
-      val send: Future[Try[Boolean]] = secretTry match {
-        case Success(secret) =>
-          val request = ws
-            .url("https://www.google.com/recaptcha/api/siteverify")
-            .withHeaders(
-              "Accept" -> "application/json",
-              "Content-type" -> "application/x-www-form-urlencoded"
-            )
-
-          val processResponse: Future[Try[Boolean]] = request.post(
-            Seq(
-              s"secret=$secret",
-              s"response=$capChaToken"
-            ).mkString("&")
-          ).map { response =>
-            Try({
-              val json = response.json
-              println(Json.stringify(json))
-              val success = (json \ "success").as[Boolean]
-              success
-            })
-          }
-
-          processResponse
-        case Failure(t) => Future {
-          Failure(t)
-        }
+      val processResponse: Future[Try[Boolean]] = request.post(
+        Seq(
+          s"secret=$secret",
+          s"response=$capChaToken"
+        ).mkString("&")
+      ).map { response =>
+        Try({
+          val json = response.json
+          println(Json.stringify(json))
+          val success = (json \ "success").as[Boolean]
+          success
+        })
       }
-      send
-    case Failure(t) => Future {
-      Failure(t)
-    }
+      processResponse
+    case Failure(t) =>
+      logger.error("Failed to load secret")
+      Future {
+        Failure(t)
+      }
   }.flatMap(identity)
 }
